2705fd33344b3aaf1d39372ed491030a53d15cd1
kggold4 <kggold4@gmail.com>
2023-07-02 23:49:55 +0300

    fix current url tab script

---
+++ load.sh
@@ -0,0 +1,3 @@
+curr_dir=$(pwd)
+echo $curr_dir
+chromium --load-extension=$curr_dir
--- popup.js
+++ popup.js
@@ -1,35 +1,46 @@
 // popup.js
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('getDataButton').addEventListener('click', getData);
-  document.getElementById('postDataButton').addEventListener('click', postData);
+document.addEventListener("DOMContentLoaded", function () {
+  let current_url;
 
-  async function getData() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      console.log(data);
-      document.getElementById('responseContainer').textContent = JSON.stringify(data);
-    } catch (error) {
-      console.error(error);
-    }
+  document
+    .getElementById("getDataButton")
+    .addEventListener("click", checkDomain);
+
+  function checkDomain() {
+    getCurrentTabUrl(handleUrl);
+    document.getElementById("responseContainer").textContent = "URL: " + current_url;
   }
 
-  async function postData() {
-    const requestData = {
-      key: 'value'
-    };
+  async function getCurrentTabUrl(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs && tabs[0]) {
+        var currentTab = tabs[0];
+        var url = currentTab.url;
+        callback(url);
+      } else {
+        console.error("Unable to retrieve the URL of the current tab.");
+      }
+    });
+  }
 
+  // Example usage
+  function handleUrl(url) {
+    if (url) {
+      current_url = url;
+    } else {
+      // todo: don't assign, just console
+      current_url = "Received an undefined URL.";
+    }
+  }
+  async function getData() {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestData)
-      });
+      const response = await fetch(
+        "https://all-news-project.github.io/example-api-data/get_similar_articles01.json"
+      );
       const data = await response.json();
       console.log(data);
-      document.getElementById('responseContainer').textContent = JSON.stringify(data);
+      document.getElementById("responseContainer").textContent =
+        JSON.stringify(data);
     } catch (error) {
       console.error(error);
     }
